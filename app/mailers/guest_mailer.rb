bfae5cb73d95097676b5a2019da8ba348b60a091
jilucev <jilucev7@gmail.com>
2014-08-13 20:47:14 +0000

    change link color

--- app/mailers/guest_mailer.rb
+++ app/mailers/guest_mailer.rb
@@ -3,13 +3,12 @@ class GuestMailer < ActionMailer::Base
   default to: "jilucev7@gmail.com"
 
   def self.call(guest)
-    guest_mail = thing(guest)
+    guest_mail = guest_message(guest)
     guest_mail.deliver
   end
 
-  def thing(guest)
+  def guest_message(guest)
     @guest = guest
-    # @url  = 'http://example.com/login'
     mail(from: "#{guest.email}", subject: "Growbus")
   end
 end
