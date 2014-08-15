00ad3c45ad8f2a6ad3b0572a421e2f520dc53607
jilucev <jilucev7@gmail.com>
2014-08-15 02:24:10 +0000

    change text and add confirmation email

--- app/mailers/guest_mailer.rb
+++ app/mailers/guest_mailer.rb
@@ -5,11 +5,20 @@ class GuestMailer < ActionMailer::Base
   def self.call(guest)
     guest_mail = guest_message(guest)
     guest_mail.deliver
+
+    guest_mail = guest_confirmation(guest)
+    guest_mail.deliver
   end
 
   def guest_message(guest)
     @guest = guest
     mail(from: "#{guest.email}", 
         subject: "Growbus")
+  end
+
+  def guest_confirmation(guest)
+    @guest = guest
+    mail(to: "#{guest.email}", 
+        subject: "bee-beep!")
   end
 end
